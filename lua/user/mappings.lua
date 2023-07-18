ae5293701e9cc6da1584a9d3f61debe890d3ebe3
einKnuffy <kjell.herzke@gmail.com>
2023-07-18 16:50:42 +0200

    main

--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -1,12 +1,20 @@
 return {
   -- first key is the mode
   n = {
+    ["<leader>b"] = false,
+    ["<leader>bb"] = false,
+    ["<leader>bd"] = false,
+    ["<leader>b\\"] = false,
+    ["<leader>b|"] = false,
+
     -- Buffer
     -- ["<leader>b"] = { name = "Buffers" },
-    ["<tab>"] = { ":bnext!<cr>", desc = "Next Tab" },
-    ["<leader>c"] = { name = "Competitive Coding" },
-    ["<leader>to"] = { ":split term://bash <cr>", desc = "Open terminal in split view" },
-    ["<leader>td"] = { ":bdelete! <cr>", desc = "Close (delete) terminal in split view" },
+    -- REMOVE THE TABS ABOVE
+    -- ["<tab>"] = { ":bnext!<cr>", desc = "Next Tab" },
+    ["<tab>"] = { ":lua require('harpoon.ui').nav_next() <cr>" },
+    ["<S-tab>"] = { ":lua require('harpoon.ui').nav_prev() <cr>" },
+    --[[   ["<leader>to"] = { ":split term://bash <cr>", desc = "Open terminal in split view" },
+    ["<leader>td"] = { ":bdelete! <cr>", desc = "Close (delete) terminal in split view" }, ]]
     -- Git
     ["<leader>gc"] = { ":!git fetch <cr> :!git add * <cr> :!git commit -m main <cr> :!git push <cr> <cr>", desc =
     "Commit all (*) changes to (main branch)" },
@@ -14,13 +22,25 @@ return {
     ["<leader>a"] = { ":AerialToggle <cr>", desc = "Toggle Aerial Window" },
     -- Code Action Menu Toggle
     ["<leader>i"] = { ':CodeActionMenu <cr>',
-      desc = "Toggle Code Action Menu", }
+      desc = "Toggle Code Action Menu", },
+    ["<leader>e"] = { name = "󱡀 Harpoon" },
+    ["<leader>ee"] = { ':lua require("harpoon.ui").toggle_quick_menu() <cr>', desc = "Toggle Harpoon" },
+    ["<leader>ea"] = { ':lua require("harpoon.mark").add_file() <cr>', desc = "Add marker to Harpoon" },
+    ["<leader>et"] = {
+      ':lua require("harpoon.term").gotoTerminal(1) <cr> i bash <cr> clear <cr>', desc =
+    "Go to (Bash) Terminal" },
+    ["<leader>ed"] = { ':lua require("harpoon.term").sendCommand(1, "pnpm dev") <cr>', desc =
+    "Start pnpm dev server" }
     -- REMINDER:
     -- Ctrl + W + [h, l] = Switch splitted open files to left or right;
     -- gcc: line comment toggle; gbc: block comment toggle
   },
+  --[[   i = {
+    ["<C-e>"] = { "exit <cr> <cr> exit <cr> <cr>", desc = "Close terminal" }
+  }, ]]
   t = {
     -- setting a mapping to false will disable it
+    ["<C-e>"] = { "<cr> exit <cr>", desc = "Close terminal" }
     -- ["<esc>"] = false,
     -- ["<leader>td"] = { "exit <cr> <cr>", desc = "Close (delete) terminal in split view" },
   },
