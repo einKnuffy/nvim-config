ab80d16ecfbc72ab93884006b1a5779d238e1e87
einKnuffy <kjell.herzke@gmail.com>
2023-08-12 22:45:07 +0200

    t

--- init.lua
+++ init.lua
@@ -207,9 +207,9 @@ require('lazy').setup({ -- NOTE: First, some plugins that don't require any conf
         neovim_image_text   = "It's neovim fr",     -- Text displayed when hovered over the Neovim image
         main_image          = "programming-1",      -- Main image display (either "neovim" or "file")
         client_id           = "992075159097843753", -- Custom sauce
-        log_level           = "debug",              -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
+        log_level           = "error",              -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
         debounce_timeout    = 10,                   -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
-        enable_line_number  = false,                -- Displays the current line number instead of the current project
+        enable_line_number  = true,                 -- Displays the current line number instead of the current project
         blacklist           = {},                   -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
         buttons             = true,                 -- Configure Rich Presence button(s), either a boolean to enable/disable, a static table (`{{ label = "<label>", url = "<url>" }, ...}`, or a function(buffer: string, repo_url: string|nil): table)
         file_assets         = {},                   -- Custom file asset definitions keyed by file names and extensions (see default config at `lua/presence/file_assets.lua` for reference)
