71f8e56ccb85ebd99a38cfc16aacfc774494a962
einKnuffy <kjell.herzke@gmail.com>
2023-10-01 10:38:39 +0200

    laast

---
+++ lua/ascii/no-place-like-home-2.lua
@@ -0,0 +1,12 @@
+return {
+	"                            there is no place like                        ",
+	"",
+	"db       .d88b.   .o88b.  .d8b.  db      db   db  .d88b.  .d8888. d888888b",
+	"88      .8P  Y8. d8P  Y8 d8' `8b 88      88   88 .8P  Y8. 88'  YP `~~88~~'",
+	"88      88    88 8P      88ooo88 88      88ooo88 88    88 `8bo.      88",
+	"88      88    88 8b      88~~~88 88      88~~~88 88    88   `Y8b.    88",
+	"88booo. `8b  d8' Y8b  d8 88   88 88booo. 88   88 `8b  d8' db   8D    88",
+	"Y88888P  `Y88P'   `Y88P' YP   YP Y88888P YP   YP  `Y88P'  `8888Y'    YP",
+	"",
+	"",
+}
--- lua/ascii/no-place-like-home.lua
+++ lua/ascii/no-place-like-home.lua
@@ -9,5 +9,16 @@ return {
 	" VP 888888D d8'     VP  `Y88P'  VP  `Y88P'  VP  VP ",
 	"",
 	"",
-	--	string.format("        %s        |     Welcome %s!      ", os.date('%H:%M'), os.getenv("USERNAME"))
+	-- string.format("        %s        |     Welcome %s!      ", os.date('%H:%M'), os.getenv("USERNAME"))
 }
+
+
+
+
+--[[
+db       .d88b.   .o88b.  .d8b.  db      db   db  .d88b.  .d8888. d888888b
+88      .8P  Y8. d8P  Y8 d8' `8b 88      88   88 .8P  Y8. 88'  YP `~~88~~'
+88      88    88 8P      88ooo88 88      88ooo88 88    88 `8bo.      88
+88      88    88 8b      88~~~88 88      88~~~88 88    88   `Y8b.    88
+88booo. `8b  d8' Y8b  d8 88   88 88booo. 88   88 `8b  d8' db   8D    88
+Y88888P  `Y88P'   `Y88P' YP   YP Y88888P YP   YP  `Y88P'  `8888Y'    YP      ]]
